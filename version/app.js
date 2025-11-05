b4f07faf3c7ecaa7a718a119f8103bd3ad02c0e8
mukhammadikhwan-cyber <mukhammad.ikhwan@students.paramadina.ac.id>
2025-11-05 20:01:18 +0700

    ok

--- version/app.js
+++ version/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello OpenShift!')
+  res.send('This is version 2 of the app.\n')
 })
 
 const port = process.env.PORT || 8080
