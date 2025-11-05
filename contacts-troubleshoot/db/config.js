04fb8fdab75968691936f2ab8a25b0a1fca2bedf
mukhammadikhwan-cyber <mukhammad.ikhwan@students.paramadina.ac.id>
2025-11-05 21:29:27 +0700

    config

---
+++ contacts-troubleshoot/db/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: process.env.PORT || 8080,
+  nodeEnv: process.env.NODE_ENV || 'development',
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  debugPort: process.env.DEBUG_PORT || 5858,
+}
