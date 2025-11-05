53cf92e2fa4c32f4f73acfb371570d21af8f99db
mukhammadikhwan-cyber <mukhammad.ikhwan@students.paramadina.ac.id>
2025-11-05 21:44:34 +0700

    ok

--- contacts-troubleshoot/app.js
+++ contacts-troubleshoot/app.js
@@ -1,41 +1,11 @@
-import React, { useEffect, useState } from 'react'
+const express = require('express')
+const app = express()
 
-function App() {
-  const [contacts, setContacts] = useState([])
+app.get('/', (req, res) => {
+  res.send('This is version 2 of the app.\n')
+})
 
-  // Fetch contacts from the backend API
-  useEffect(() => {
-    fetch('http://localhost:8080/contacts') // Adjust URL for production
-      .then((response) => response.json())
-      .then((data) => setContacts(data))
-      .catch((error) => console.error('Error fetching contacts:', error))
-  }, [])
-
-  return (
-    <div>
-      <h1>Contact List</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{contact.firstName}</td>
-              <td>{contact.lastName}</td>
-              <td>{contact.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
-export default App
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
