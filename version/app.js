b360353145468d7e286bd31f159012a96e1f7865
mukhammadikhwan-cyber <mukhammad.ikhwan@students.paramadina.ac.id>
2025-11-05 19:54:51 +0700

    ok

--- version/app.js
+++ version/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
 
-// route utama
 app.get('/', (req, res) => {
   res.send('Hello OpenShift!')
 })
 
+const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
